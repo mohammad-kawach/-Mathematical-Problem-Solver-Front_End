38b8220bc070e06a1cade24a74516d7da0fa6a86
mohammad-kawach <mohmad.kawach.777@gmail.com>
2025-01-18 12:36:38 +0100

    small changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,10 +2,10 @@ export const Footer = () => {
   return (
     <footer className="footer">
       <div className="footer-links">
-        <a href="https://github.com/yourusername/math-assistant">
+        <a href="https://github.com/mohammad-kawach/Mathematical-Problem-Solver-Front_End">
           Front-End GitHub Repo
         </a>
-        <a href="https://github.com/yourusername/math-assistant">
+        <a href="https://github.com/mohammad-kawach/Mathematical-Problem-Solver-API">
           Back-End GitHub Repo
         </a>
       </div>
